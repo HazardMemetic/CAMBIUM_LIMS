668862c1efffadbe929b51bc25d44ed492ac2fcc
CambiumDan <43409905+CambiumDan@users.noreply.github.com>
2018-09-21 09:04:34 -0400

    New features (#7)
    * Removed inline styling + added consistent spacing
    
    * Added bootstrap template initial files
    
    I put all of the bootstrap template files in the Template Demo folder. Also has the concrete cylinder page started but not completed.
    
    * Most recent update
    
    1. Still need to remove the delete button
    2. Not responsive, only looks good on large screens (1200 px+)

---
+++ Template Demo/script.js
@@ -0,0 +1,26 @@
+//var inputRow = [];
+//
+//var displayDelete = "";
+//
+//var test = "";
+//
+//
+//
+//
+//
+//var test = document.getElementsByClassName("tableButton");
+//
+//
+//
+//console.log(test);
+//
+//console.log(test.input.tableButton);
+//
+//
+//
+//
+////if (inputRow.length === 1) {
+////    document.getElementsByClassName("tableButton").style.display = "none";
+////} else {
+////    document.getElementsByClassName("tableButton").style.display = "block";
+////}
